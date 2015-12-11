200326aa5a84b2bb7300ba609d11d332ce9a669c
khyron <khyron@spyder.headup.ws>
2015-12-10 20:09:58 -0600

    Forget to push some files

--- README.md
+++ README.md
@@ -8,3 +8,48 @@ For a group of IPs - all switches are going to be patched
 
 Make sure to comment and uncomment the right option inside the .py file
 The default is a group of switches
+
+Setting the environment
+===========
+- Install Virtual Environments 
+
+    http://docs.python-guide.org/en/latest/dev/virtualenvs/
+- Activate the venv
+
+    **$ source .venv/bin/activate**
+- Install the requeriments
+
+    **$ pip install -r requirements.txt**
+
+Running the script
+===========
+- Execute to patch only one host
+
+    **$ python autopatch.py --hostname 10.0.1.139 --username khyron --password
+$(cat /tmp/key) --ftpserver ftpserver.local
+
+- Execute to patch multiple using hostlist.csv list
+    **$ python autopatch.py --username khyron --password $(cat
+/tmp/key) --hostlist hostlist.csv --ftpserver ftpserver.local
+
+Script help
+===========
+
+<pre>
+$ python autopatch.py -h
+usage: autopatch.py [-h] [--hostname HOSTNAME] --ftpserver FTPSERVER
+                    [--port PORT] --username USERNAME --password PASSWORD
+                    [--hostlist HOSTLIST]
+
+Using a hostlist and an ftp server patch ARISTA switches.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --hostname HOSTNAME   Single switch to path (ignored if --hostlist is used)
+  --ftpserver FTPSERVER
+                        FTP server where path file is stored
+  --port PORT           port where sshd is running (default 22)
+  --username USERNAME   Username to connec to switch
+  --password PASSWORD   Password to connect to switch
+  --hostlist HOSTLIST   Path to hostlist in CSV format
+</pre>
--- autopatch.py
+++ autopatch.py
@@ -1,65 +1,88 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from Exscript.protocols import SSH2
 from Exscript import Account
 from time import sleep
+import argparse
 import csv
+import sys
 import re
 
-"""
-# OPCION CON UN SOLO HOST. LA IP DE ESTA DECLARADA EN LA VARIABLE IP
 
-ip='192.168.1.1'
-username='##USERNAME##'
-password='##PASSWORD##'
+def patch(hostname, ftpserver, username, password, sleep):
+    try:
+        print "[+] Patching host => IP: %s" % (repr(hostname))
+        account = Account(username, password)
+        conn = SSH2()
+        conn.connect("%s" % hostname)
+        conn.login(account)
+        conn.execute("copy ftp://%s/secAdvisory0015.swix flash:" % ftpserver)
+        sleep(sleep)
+        conn.execute('copy flash:secAdvisory0015.swix extension:')
+        sleep(sleep)
+        conn.execute('extension secAdvisory0015.swix')
+        sleep(sleep)
+        conn.execute('copy installed-extensions boot-extensions')
+        sleep(sleep)
+        conn.execute('delete secAdvisory0015.swix')
+        print "[+] Patching host OK => IP: %s\n" % (repr(hostname))
+        conn.send('exit\r')
+        conn.close()
+    except Exception as e:
+        if conn:
+            conn.close()
+        print e
+        print "[-] Error patching host => IP: %s" % (repr(hostname))
 
-account = Account(username,password)
-conn = SSH2()
 
-conn.connect('%s' % ip)
-conn.login(account)
-conn.execute('copy ftp://ftpserver.local/secAdvisory0015.swix flash:')
-sleep(1)
-conn.execute('copy flash:secAdvisory0015.swix extension:')
-sleep(1)
-conn.execute('extension secAdvisory0015.swix')
-sleep(1)
-conn.execute('copy installed-extensions boot-extensions')
-sleep(1)
-conn.execute('delete secAdvisory0015.swix')
-conn.send('exit\r')
-conn.close()
-"""
+def csv_read(path, switches):
+    with open(path, 'rb') as f:
+        reader = csv.reader(f)
+        try:
+            for row in reader:
+                switch = {}
+                switch["description"] = row[0]
+                switch["hostname"] = row[1]
+                switch["ip"] = row[2]
+                switches.append(switch)
+        except csv.Error as e:
+            exit('[-] File %s, line %d: %s' % (filename, reader.line_num, e))
+    return switches
 
-# OPCION IMPORTANDO LOS HOSTS DESDE UN CSV LLAMADO HOSTLIST
 
-username='##USERNAME##'
-password='##PASSWORD##'
+def args_parse():
+    parser = argparse.ArgumentParser(description='Using a hostlist and an\
+            ftp server patch ARISTA switches.')
+    parser.add_argument("--hostname", type=str, help='Single switch to path\
+            (ignored if --hostlist is used)')
+    parser.add_argument("--ftpserver", type=str, required=True, help='FTP server\
+            where path file is stored')
+    parser.add_argument("--port", default="22", type=str, help='port where sshd\
+            is running (default 22)')
+    parser.add_argument("--username", type=str, required=True, help='Username \
+            to connec to switch')
+    parser.add_argument("--password", type=str, required=True, help='Password \
+            to connect to switch')
+    parser.add_argument("--hostlist", type=str, help='Path to hostlist in \
+            CSV format')
+    args = parser.parse_args()
 
-with open('hostlist.csv', 'rb') as f:
-    reader=csv.reader(f)
-    host_temp_list=list(reader)
+    return args
 
-account = Account(username,password)
-conn = SSH2()
 
-for host in host_temp_list:
-	try:
-		print "se comienza a ejecutar en el host ", repr(host[0])
-		conn.connect('%s'%host[0])
-		conn.login(account)
-		conn.execute('copy ftp://ftpserver.local/secAdvisory0015.swix flash:')
-		sleep(2)
-		conn.execute('copy flash:secAdvisory0015.swix extension:')
-		sleep(1)
-		conn.execute('extension secAdvisory0015.swix')
-		sleep(1)
-		conn.execute('copy installed-extensions boot-extensions')
-		sleep(1)
-		conn.execute('delete secAdvisory0015.swix')
-		print "se ejecuto correctamente todo el script en el host", repr(host[0])
-		print ""
-		conn.send('exit\r')
-		conn.close()
-	except:
-		print "Algo fallo"
+def main():
+    switches = []
+    args = args_parse()
+
+    if args.hostlist:
+        switches = csv_read(args.hostlist, switches)
+        for switch in switches:
+            patch(switch["ip"], args.ftpserver, args.username, args.password, 2)
+    elif args.hostname:
+        patch(args.hostname, args.ftpserver, args.username, args.password, 2)
+
+
+# Start program
+if __name__ == "__main__":
+    main()
